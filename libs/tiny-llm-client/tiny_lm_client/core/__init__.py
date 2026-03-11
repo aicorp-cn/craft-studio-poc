3ed8c5c5cb1be97ecb76c9c5b251ad6b3f8d9f85
GenjiLuo <luogenji@hotmail.com>
2026-03-11 12:19:24 +0800

    chore: configure tiny-lm-client as submodule
    - Update .gitmodules to use correct repository URL
    - Replace embedded tiny-llm-client with submodule reference

--- libs/tiny-llm-client/tiny_lm_client/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""核心包 - 包含客户端主类"""
-
-from .client import TinyLMClient
-from .task_client import TinyTaskClient
-
-__all__ = ['TinyLMClient', 'TinyTaskClient']
--- libs/tiny-llm-client/tiny_lm_client/parsers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""解析器包 - 包含响应解析功能"""
-
-from .response_parser import ResponseParser
-
-__all__ = ['ResponseParser']
